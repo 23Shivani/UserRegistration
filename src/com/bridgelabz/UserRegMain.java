c8440bdc31087c7df9902985a0c84ffa285101bf
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-29 10:11:11 +0530

    Added: First Name Validate Program

---
+++ src/com/bridgelabz/UserRegMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegMain {
+	public static void main(String[] args) {
+		UserRegistrationImpl reg = new UserRegistrationImpl();
+		reg.firstNameValidate();
+	}
+
+}
